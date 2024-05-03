2871ba45498d6034dabf28e81b7d9b12e4f3e81e
JP Mohr <James.Mohr2.0@hotmail.com>
2024-05-03 16:08:14 -0400

    Overload Exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -5,6 +5,28 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            
+        }
+        public static int Add(int num1, int num2)
+        {
+            int total = num1 + num2;
+            return total;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            decimal total = num1 + num2;
+            return total;
+        }
+
+        public static string Add(int num1, int num2, bool isUSD)
+        {
+            if (isUSD)
+            {
+                string total = (num1 + num2).ToString();
+                return total;
+            }
+            else { return "no funny money"; }
         }
     }
 }
